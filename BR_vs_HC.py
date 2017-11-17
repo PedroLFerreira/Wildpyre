35649d75549138f5a6830c29736f50cf9ac02dde
Antonio <acapela96@hotmail.com>
2017-11-17 20:49:27 +0000

    all the plots

--- BR_vs_HC.py
+++ BR_vs_HC.py
@@ -13,20 +13,20 @@ T[int(nx/2):int(nx/2+2),int(ny/2):int(ny/2+3)] = 1000      # Increase temperatur
 F = np.random.normal(loc=500, scale=0, size=(nx,ny))
 F[0,:] = 0; F[-1,:] = 0;F[:,-1] = 0; F[:,0] = 0
 
-heatContentX = np.linspace(0,30,11)#[20 + i for i in range(10)]
-TcritX = np.linspace(100,0,11) #[150 + 10*i for i in range(10)]
+burnX = np.linspace(0.001,0.011,11)#[20 + i for i in range(10)]
+heatX = np.linspace(0,30,11) #[150 + 10*i for i in range(10)]
 
-results = np.zeros((len(TcritX), len(heatContentX)))
-dead = np.zeros((len(TcritX), len(heatContentX)), dtype=bool)
+results = np.zeros((len(heatX), len(burnX)))
+dead = np.zeros((len(heatX), len(burnX)), dtype=bool)
 
-for i in range(len(TcritX)):
-    for j in range(len(heatContentX)):
+for i in range(len(heatX)):
+    for j in range(len(burnX)):
         for k in range(1):
             F = np.random.normal(loc=500, scale=0, size=(nx,ny))
             sim = Simulator(nx,ny,nt,T=T.copy(),A=A.copy(),F=F.copy(),H=np.zeros((nx,ny)), dt=1,
-                            Tcrit=TcritX[i],
-                            burningRate=0.005,
-                            heatContent=heatContentX[j],
+                            Tcrit=150,
+                            burningRate=burnX[j],
+                            heatContent=heatX[i],
                             planarDiffusivity=0.005,
                             atmosphericDiffusivity=.004,
                             slopeContribution=1)        # Initialize fields and parameters
@@ -36,7 +36,7 @@ for i in range(len(TcritX)):
             print(sim.Metrics()['burntFuel'])
             print(sim.Metrics()['dead'])
             #print(sim.Metrics()['burntStep'][-10:])
-            print((i*len(heatContentX) + j + 1)/(len(heatContentX) * len(TcritX))*100, '%', end='\r')
+            print((i*len(burnX) + j + 1)/(len(burnX) * len(heatX))*100, '%', end='\r')
 
 print(dead)
 
@@ -51,10 +51,10 @@ img = plt.imshow(results/1, cmap=cmap, origin='lower', interpolation='bicubic')
 plt.colorbar(img)
 plt.clim(0, 1)
 skip = 3
-plt.yticks(range(0,len(TcritX), skip)      , TcritX[::skip])
-plt.xticks(range(0,len(heatContentX), skip), heatContentX[::skip])
-plt.ylabel('$T_{crit}$')
-plt.xlabel('$\lambda_{HC}$')
+plt.yticks(range(0,len(heatX), skip)      , heatX[::skip])
+plt.xticks(range(0,len(burnX), skip), burnX[::skip])
+plt.ylabel('$\lambda_{HC}$')
+plt.xlabel('$\lambda_{BR}$')
 plt.title('Fraction of area burnt in {} steps'.format(nt))
 
 plt.show()
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ A = A/np.max(A)
 A = np.zeros((100,100))
 nx=A.shape[0];ny=A.shape[0];nt=2000;                         # Fundamental simulation parameters
 T = np.zeros((nx,ny))                                    # Initialize T field
-T[int(nx/2):int(nx/2+2),int(ny/2):int(ny/2+3)] = 1000      # Increase temperature in the middle of the grid
-F = np.random.normal(loc=500, scale=200, size=(nx,ny))
+#T[int(nx/2):int(nx/2+2),int(ny/2):int(ny/2+3)] = 1000      # Increase temperature in the middle of the grid
+T[nx//2, ny//2:ny//2+2] = 1000
+F = np.random.normal(loc=500, scale=0, size=(nx,ny))
+F = np.maximum(F, 0)
 
 sim = Simulator(nx,ny,nt,T=T,A=A,F=F,dt=1,
                 Tcrit=150,
