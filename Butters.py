d28ba6ac20a7af015558195e31481c80f2ae21eb
pedroalexxxferreira <pedroalexxxferreira@gmail.com>
2017-11-28 18:27:31 +0000

    made Butters.py cleaner

--- Butters.py
+++ Butters.py
@@ -4,16 +4,14 @@ from scipy import misc
 import matplotlib.pyplot as plt
 
 Terrain = misc.imread('Terrain.png').T
-#Terrain[850:1000,150:300] = 0
 plt.imshow(Terrain,cmap='gray',origin='lower')
 plt.show()
 A = misc.imread('Terrain.png').T[850:1000,150:300]
 plt.imshow(A,cmap='gray',origin='lower')
 plt.colorbar()
 plt.show()
-nx=A.shape[0];ny=A.shape[0];nt=20000;                         # Fundamental simulation parameters
+nx=A.shape[0];ny=A.shape[0];nt=20000;                    # Fundamental simulation parameters
 T = np.zeros((nx,ny))                                    # Initialize T field
-#T[int(nx/2):int(nx/2+2),int(ny/2):int(ny/2+3)] = 1000      # Increase temperature in the middle of the grid
 T[nx//2, ny//2:ny//2+2] = 1000
 F = np.random.normal(loc=500, scale=0, size=(nx,ny))
 F = np.maximum(F, 0)
@@ -25,11 +23,10 @@ sim = Simulator(nx,ny,nt,T=T,A=A,F=F,dt=.01,
                 planarDiffusivity=0.005,
                 atmosphericDiffusivity=0.001,
                 slopeContribution=0.001)         # Initialize fields and parameters
-#sim.Run(animStep=20,
-#        Tclim=(0,800))                         # Perform the simulation
-#sim.Show()                                  # Visualize the results
+#sim.Run(animStep=20, Tclim=(0,800))             # Perform the simulation
+#sim.Show()                                      # Visualize the results
 
-sim.CreateGIF(skip=400,maxIterations = 1500,
+sim.CreateGIF(skip=400, maxIterations = 1500,
                         Tclim=(0,500),
                         Hclim=(0,7),
-                        TOnly=True)
+                        TOnly=True) # Creates video file
